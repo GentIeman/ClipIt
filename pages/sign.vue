57e4b8cde77898ac1a1b4bfa4cd7099e499848eb
ilyas <ilyashepelev23@gmail.com>
2025-06-29 23:54:34 +0300

    initial

---
+++ pages/sign.vue
@@ -0,0 +1,17 @@
+<template>
+  <UContainer>
+    <UForm :state="{}">
+      <UFormField label="Email">
+        <UInput placeholder="Enter your email" />
+      </UFormField>
+    </UForm>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
