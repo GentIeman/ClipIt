25855f1154ca0d7099759dd36a17c119c4de34eb
ilyas <ilyashepelev23@gmail.com>
2025-07-09 22:58:33 +0300

    feat(content): add forms collection

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,28 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
     collections: {
-        content: defineCollection({
-            type: 'page',
-            source: '**/*.json'
-        })
+        forms: defineCollection({
+            type: 'data',
+            source: 'forms/*.json',
+            schema: z.object({
+                legend: z.string(),
+                fields: z.array(
+                    z.object({
+                        name: z.string(),
+                        label: z.string(),
+                        placeholder: z.string(),
+                        size: z.enum(["sm", "md", "lg"])
+                    })
+                ),
+                button: z.object({
+                    label: z.string(),
+                    block: z.boolean(),
+                    size: z.enum(["sm", "md", "lg"]),
+                    type: z.enum(["submit", "button"]),
+                }),
+                required: z.array(z.string())
+            })
+        }),
     }
 })
