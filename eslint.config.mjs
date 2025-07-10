e0291d468177176228fcc38a36a48950f29adeef
ilyas <ilyashepelev23@gmail.com>
2025-07-10 23:24:38 +0300

    feat(rules): added rules for ordering attrs and max number of attrs per line

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,33 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt(
+  {
+      files: ['**/*.vue'],
+      rules: {
+          "vue/attributes-order": ["error", {
+              "order": [
+                  "DEFINITION",
+                  "LIST_RENDERING",
+                  "CONDITIONALS",
+                  "RENDER_MODIFIERS",
+                  "GLOBAL",
+                  ["UNIQUE", "SLOT"],
+                  "TWO_WAY_BINDING",
+                  "OTHER_DIRECTIVES",
+                  "OTHER_ATTR",
+                  "EVENTS",
+                  "CONTENT"
+              ],
+              "alphabetical": false
+          }],
+          "vue/max-attributes-per-line": ["error", {
+              "singleline": {
+                  "max": 1
+              },
+              "multiline": {
+                  "max": 1
+              }
+          }]
+      }
+  }
+)
