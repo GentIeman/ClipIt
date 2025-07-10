2e1b2b282b4e3dfa5a1e692e6b6b26e1d2abae06
ilyas <ilyashepelev23@gmail.com>
2025-07-10 23:26:46 +0300

    fix(template): page template has been changed

--- pages/sign.vue
+++ pages/sign.vue
@@ -1,10 +1,27 @@
 <template>
-  <UContainer>
-    <UForm :state="{}">
-      <UFormField label="Email">
-        <UInput placeholder="Enter your email" />
-      </UFormField>
-    </UForm>
+  <UContainer class="flex justify-center items-center h-dvh">
+    <p class="flex flex-col p-8 items-center lg:justify-center absolute text-[30px] lg:text-[250px] text-neutral-800 select-none h-dvh">
+      POCKET<span class="underline decoration-primary-500">ALTERNATIVE</span>
+    </p>
+    <UContainer class="grid gap-4 p-10 w-lg h-fit rounded-xl border border-neutral-700 backdrop-blur-md">
+      <DynamicForm
+          v-if="form"
+          :state="state"
+          :schema="form"
+          class="grid gap-4 h-fit"
+          :validation-schema="validationSchema"/>
+      <UContainer class="lg:px-0">
+        <USeparator label="or"/>
+        {{ isSignIn ? "I don't have an": "I have an" }}
+        <UButton
+            to="#"
+            class="px-0 text-md"
+            variant="link"
+            label="account"
+            color="primary"
+            @click="isSignIn = !isSignIn"/>
+      </UContainer>
+    </UContainer>
   </UContainer>
 </template>
 
