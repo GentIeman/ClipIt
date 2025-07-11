86d9e4b87a37b23250bb3cef3fb505f699a8705c
ilyas <ilyashepelev23@gmail.com>
2025-07-11 09:38:08 +0300

    hotfix(schema): swap validation order

--- layers/form/validators/authRules.ts
+++ layers/form/validators/authRules.ts
@@ -4,19 +4,19 @@ export const signInSchema = yup.object({
     email: yup
         .string()
         .email('Please enter a valid email address')
-        .min(6, "Email must be at least 6 characters long")
-        .required('Email is required'),
+        .required('Email is required')
+        .min(6, "Email must be at least 6 characters long"),
     password: yup
         .string()
-        .min(6, 'Password must be at least 6 characters long')
         .required('Password is required')
+        .min(6, 'Password must be at least 6 characters long')
 });
 
 export const signUpSchema = yup.object({
     username: yup
         .string()
-        .min(6, "Username must be at least 6 characters long")
-        .required('Username is required'),
+        .required('Username is required')
+        .min(6, "Username must be at least 6 characters long"),
     email: yup
         .string()
         .min(6, "Email must be at least 6 characters long")
@@ -24,6 +24,6 @@ export const signUpSchema = yup.object({
         .required('Email is required'),
     password: yup
         .string()
-        .min(6, 'Password must be at least 6 characters long')
         .required('Password is required')
+        .min(6, 'Password must be at least 6 characters long')
 });
