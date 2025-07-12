52542001b3466cec2e439f5ddbc5f2fd11b9c68d
ilyas <ilyashepelev23@gmail.com>
2025-07-12 18:33:20 +0300

    feat(template): add bookmark button

--- pages/bookmarks.vue
+++ pages/bookmarks.vue
@@ -1,5 +1,15 @@
 <template>
-  <div/>
+  <main>
+    <div class="fixed bottom-0 w-full grid sm:hidden place-items-center my-3">
+      <UButton
+          label="New Bookmark"
+          color="primary"
+          size="md"
+          leading-icon="i-lucide-plus"
+          variant="solid"
+      />
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
