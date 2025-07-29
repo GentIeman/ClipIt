d427e009c4b90c1d2b2233f2418a77960659eaac
ilyas <ilyashepelev23@gmail.com>
2025-07-29 20:53:43 +0300

    feat(form): form rendering is implemented

--- layers/ui/components/AddBookmarkModal.vue
+++ layers/ui/components/AddBookmarkModal.vue
@@ -9,10 +9,34 @@
         leading-icon="i-lucide-plus"
         variant="solid"
     />
+    <template #body>
+      <DynamicForm
+          v-if="form"
+          class="grid gap-4 h-fit"
+          :state="state"
+          :validation-schema="bookmarkSchema"
+          :schema="form"/>
+    </template>
   </UModal>
 </template>
 
 <script setup lang="ts">
+import {bookmarkSchema} from "~/layers/bookmarks/validators/bookmarkRules";
+
+const user = useStrapiUser()
+
+const state = reactive({
+  link: "",
+  user: user.value?.id
+})
+
+const {data: form} = useAsyncData("form",
+    () => queryCollection('forms')
+        .where("stem", "=", "forms/bookmark")
+        .select("button", "fields")
+        .first()
+)
+
 </script>
 
 <style scoped>
