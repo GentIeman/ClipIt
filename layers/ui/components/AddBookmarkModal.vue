6ca6af9f7a6066d1b0f64ce37ebf4445e20d5ec0
ilyas <ilyashepelev23@gmail.com>
2025-07-29 19:04:51 +0300

    initial

---
+++ layers/ui/components/AddBookmarkModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <UModal
+      title="New bookmark"
+      description="Save your favorite links for quick access later">
+    <UButton
+        label="New"
+        color="primary"
+        size="md"
+        leading-icon="i-lucide-plus"
+        variant="solid"
+    />
+  </UModal>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
