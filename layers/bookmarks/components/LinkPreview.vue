f63339d6306197fe22609eb03a227ee1bfc60896
ilyas <ilyashepelev23@gmail.com>
2025-08-02 12:19:18 +0300

    initial

---
+++ layers/bookmarks/components/LinkPreview.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <p class="text-primary font-bold">{{ preview?.siteName }}</p>
+    <h1 class="font-bold">{{ preview?.title }}</h1>
+    <h2 class="line-clamp-2 text-neutral-300">{{ preview?.description }}</h2>
+    <img
+        v-if="preview?.image"
+        :src="preview?.image"
+        alt="Link preview">
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  preview: ILinkPreview
+}>()
+</script>
