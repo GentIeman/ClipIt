677471564f179f88e4062ac3f09eedfb1fda3145
ilyas <ilyashepelev23@gmail.com>
2025-08-04 16:24:57 +0300

    refactor(fetchPreview): using toValue method

--- layers/bookmarks/composables/useLinkPreview.ts
+++ layers/bookmarks/composables/useLinkPreview.ts
@@ -1,4 +1,4 @@
-import { shallowRef } from 'vue'
+import { shallowRef, toValue } from 'vue'
 
 export function useLinkPreview() {
     const preview = shallowRef<ILinkPreview | null>(null)
@@ -11,9 +11,10 @@ export function useLinkPreview() {
 
     const fetchPreview = async (url: string) => {
         error.value = null
+        const targetUrl = toValue(url)
 
         try {
-            const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+            const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(targetUrl)}`)
             const data = await response.json()
             const html = data.contents
 
