5f62abbbfe4e0e6919e3090407294daa9eee8d71
ilyas <ilyashepelev23@gmail.com>
2025-08-27 17:12:59 +0300

    chore: ssr and devtools are disabled

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
-    devtools: {enabled: true},
+    devtools: {enabled: false},
     modules: [
         '@nuxt/test-utils',
         '@nuxt/ui',
@@ -24,5 +24,6 @@ export default defineNuxtConfig({
     },
     routeRules: {
         '/': {redirect: '/bookmarks'}
-    }
+    },
+    ssr: false
 })
