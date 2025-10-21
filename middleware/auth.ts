a63129eb8285e237934ea1c187d085123811bfe4
ilyas <ilyashepelev23@gmail.com>
2025-10-21 19:40:17 +0300

    deleted

--- middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, _from) => {
-    const user = useStrapiUser()
-    if (!user.value) {
-        useCookie('redirect', { path: '/', httpOnly: true, secure: true, sameSite: 'strict' }).value = to.fullPath
-        return navigateTo('/sign')
-    }
-})
