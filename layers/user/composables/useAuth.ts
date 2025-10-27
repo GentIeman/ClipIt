a7bbf2346a91d23a5186cded3f3a7f2dad8ea02a
ilyas <ilyashepelev23@gmail.com>
2025-10-27 14:58:11 +0300

    update: try/catch block removed

--- layers/user/composables/useAuth.ts
+++ layers/user/composables/useAuth.ts
@@ -2,26 +2,25 @@ export const useAuth = () => {
     const user = useUser();
 
     const login = async (identifier: string, password: string) => {
-        try {
-            user.value = await $fetch<User>("/api/login", {
-                method: "POST",
-                body: {identifier, password},
-            });
-
-        } catch (error) {
-            console.error(error)
-        }
+        user.value = await $fetch<User>("/api/login", {
+            method: "POST",
+            body: {identifier, password},
+            // async onResponseError({ response } ) {
+            //     console.log(response._data.data.error.status)
+            //     console.log(response._data.data.error.message)
+            // }
+        });
     };
 
     const register = async (email: string, password: string, username: string) => {
-        try {
-            user.value = await $fetch<User>("/api/register", {
-                method: "POST",
-                body: {email, password, username},
-            });
-        } catch (error) {
-            console.error(error)
-        }
+        user.value = await $fetch<User>("/api/register", {
+            method: "POST",
+            body: {email, password, username},
+            // async onResponseError({ response } ) {
+            //     console.log(response._data.data.error.status)
+            //     console.log(response._data.data.error.message)
+            // }
+        });
     };
 
     return {login, register};
