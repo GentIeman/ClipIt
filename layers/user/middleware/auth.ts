9e603f47ebe842977d2b032b404957313e5f2147
ilyas <ilyashepelev23@gmail.com>
2025-11-14 14:28:15 +0300

    feat: call me() to refresh user data before auth check

--- layers/user/middleware/auth.ts
+++ layers/user/middleware/auth.ts
@@ -1,5 +1,12 @@
+import {useAuth} from "~/layers/user/composables/useAuth"
 export default defineNuxtRouteMiddleware(async (to, _from) => {
     const user = useUser()
+
+    const {me} = useAuth()
+
+    if (!user.value) {
+        await me()
+    }
 
     if (!user.value) {
         useCookie('redirect', { path: '/', httpOnly: true, secure: true, sameSite: 'strict' }).value = to.fullPath
